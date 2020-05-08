94207c141e82683c95b3d4a3ef06570ddade7bfb
Evgeny Korobenkov <joekor463@gmail.com>
2020-05-08 21:42:48 +0800

    sum checker

---
+++ EqualSumChecker.java
@@ -0,0 +1,8 @@
+public class EqualSumChecker {
+    public static boolean hasEqualSum(int First, int Second, int Equal) {
+        int Summ = First + Second;
+        if (Summ == Equal) {
+            return true;
+        } else return false;
+    }
+}
---
+++ MAin.java
@@ -0,0 +1,7 @@
+public class MAin {
+    public static void main (String args[]){
+        boolean b = EqualSumChecker.hasEqualSum(1, -1, 0);
+        System.out.println(b);
+    }
+
+}
